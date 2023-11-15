22960355fe8497665329b6b5effc7499c14fc705
JesseDexplorer <146500060+JesseDexplorer@users.noreply.github.com>
2023-11-14 21:40:27 -0600

    set up webdriver lifecycle using junit and PageFactory class

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,25 @@
+import code.assement.PageFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
 public class MainTest {
+    @Before
+    public void setUp(){
+        WebDriver driver = PageFactory.getDriver();
+
+    }
+
+
+    @Test
+    public void testCaseOne(){
+        System.out.println("set up");
+    }
+
+    @After
+    public void tearDown(){
+        PageFactory.quitDriver();
+
+    }
 }
