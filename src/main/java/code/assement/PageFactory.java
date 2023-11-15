6aa3013057c7d7804663d655bb84c77de43f9718
JesseDexplorer <146500060+JesseDexplorer@users.noreply.github.com>
2023-11-14 21:45:43 -0600

    created methods to handle assertions with a time stamp

--- src/main/java/code/assement/PageFactory.java
+++ src/main/java/code/assement/PageFactory.java
@@ -4,6 +4,11 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class PageFactory {
     private static WebDriver driver;
 
@@ -17,6 +22,24 @@ public class PageFactory {
         }
         return driver;
     }
+
+    // create a log file for tracking Assertions
+    public static void writeToAssertionErrorsFile(String errorMessage, AssertionError e){
+        try (FileWriter writer = new FileWriter("assertion_errors.txt", true)) {
+            writer.write(getFormattedTimestamp() + "\n" + e + errorMessage); // adds the time stamp to the error message
+        } catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+
+    // Helper method to get a formatted timestamp
+    private static String getFormattedTimestamp(){
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM-dd-yyyy HH:mm");
+        return now.format(formatter);
+    }
+
 
     public static void quitDriver(){
         if(driver != null){
