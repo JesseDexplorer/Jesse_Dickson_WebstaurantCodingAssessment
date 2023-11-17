6c9c96af8072ccb42c8b333f60943dff346029f4
JesseDexplorer <146500060+JesseDexplorer@users.noreply.github.com>
2023-11-16 23:56:29 -0600

    increase wait time to 30 seconds in the case load times are longer

--- src/main/java/code/assement/ShoppingPage.java
+++ src/main/java/code/assement/ShoppingPage.java
@@ -18,7 +18,7 @@ public class ShoppingPage {
 
     public ShoppingPage(WebDriver driver){
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, 10);
+        this.wait = new WebDriverWait(driver, 30);
     }
 
     /**
